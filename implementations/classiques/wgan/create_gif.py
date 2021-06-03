e4ce75be7e75b987be922aa3628aa3e26f7b382e
antdasneves <antoine_dasneves@yahoo.fr>
2021-06-03 14:16:27 +0200

    oui

--- implementations/classiques/wgan/create_gif.py
+++ implementations/classiques/wgan/create_gif.py
@@ -8,9 +8,6 @@ for i in imgs:
     new_frame = Image.open(i)
     frames.append(new_frame)
 
-print(frames[:10])
-frames=sorted(frames)
-print(frames)
 # Save into a GIF file that loops forever
 frames[0].save('/neurospin/dico/adneves/wgan_gp/exploration2/exploration1.gif', format='GIF',
                append_images=frames,
